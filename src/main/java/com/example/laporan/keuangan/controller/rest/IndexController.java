4732e0f896afddd17f63f0c07ac130923592f1d3
ridlwanyunus <hiddenjustice309@gmail.com>
2024-01-01 07:17:19 +0700

    Sorting year and month

--- src/main/java/com/example/laporan/keuangan/controller/rest/IndexController.java
+++ src/main/java/com/example/laporan/keuangan/controller/rest/IndexController.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -183,6 +185,21 @@ public class IndexController {
 
 		List<Saldo> listSaldo = saldoService.findAllWithTransaction();
 
+		Collections.sort(listSaldo, new Comparator<Saldo>() {
+
+			@Override
+			public int compare(Saldo o1, Saldo o2) {
+				// TODO Auto-generated method stub
+				int date1 = o1.getTahun() + o1.getBulan();
+				int date2 = o2.getTahun() + o2.getBulan();
+				
+				return date1 - date2;
+			}
+
+		});
+		
+		
+		
 		response.setStatus(1);
 		response.setMessage("Success load history");
 		response.setData(listSaldo);
